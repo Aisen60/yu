41a40b6892b5d42da49ef55c1e72f13cea46e3ea
Aisen60 <caixusheng60@gmail.com>
2020-10-07 00:36:40 +0800

    chore: gridsome 基本配置

--- gridsome.config.js
+++ gridsome.config.js
@@ -2,9 +2,47 @@
 // Learn more: https://gridsome.org/docs/config
 
 // Changes here require a server restart.
-// To restart press CTRL + C in terminal and run `gridsome develop`
+// To restart press CTRL + C in terminpnal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Gridsome',
-  plugins: []
-}
+  icon: "./static/favicon.png",
+  titleTemplate: "aisen60 | %s",
+  plugins: [
+    {
+      use: "@gridsome/source-strapi",
+      options: {
+        apiURL: "http://caixusheng.com:1337",
+        queryLimit: 1000, // Defaults to 100
+        contentTypes: ["post", "tag", "classes"],
+        typeName: "Strapi",
+        singleTypes: ["general"],
+        // Possibility to login with a Strapi user,
+        // when content types are not publicly available (optional).
+        // loginData: {
+        //   identifier: "",
+        //   password: "",
+        // },
+      },
+    },
+  ],
+  templates: {
+    StrapiPost: [
+      {
+        path: "/post/:id",
+        component: "./src/templates/Post.vue",
+      },
+    ],
+    StrapiTag: [
+      {
+        path: "/tag/:id",
+        component: "./src/templates/Tag.vue",
+      },
+    ],
+    StrapiClasses: [
+      {
+        path: "/class/:id",
+        component: "./src/templates/Class.vue",
+      },
+    ],
+  },
+};
